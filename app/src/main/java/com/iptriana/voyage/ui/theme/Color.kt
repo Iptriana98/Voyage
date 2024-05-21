dd21addf3326cf64a94e6aaa51f709bb0ffd31de
ibrahinperez <iptrianaa@gmail.com>
2024-05-21 11:33:47 -0500

    created application theme

--- app/src/main/java/com/iptriana/voyage/ui/theme/Color.kt
+++ app/src/main/java/com/iptriana/voyage/ui/theme/Color.kt
@@ -1,11 +1,29 @@
 package com.iptriana.voyage.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val voyage_caption = Color.DarkGray
+val voyage_divider_color = Color.LightGray
+private val voyage_red = Color(0xFFE30425)
+private val voyage_white = Color.White
+private val voyage_purple_700 = Color(0xFF720D5D)
+private val voyage_purple_800 = Color(0xFF5D1049)
+private val voyage_purple_900 = Color(0xFF4E0D3A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val voyageColors = lightColorScheme(
+    primary = voyage_purple_800,
+    secondary = voyage_red,
+    surface = voyage_purple_900,
+    onSurface = voyage_white,
+    inversePrimary = voyage_purple_700
+)
+
+val BottomSheetShape = RoundedCornerShape(
+    topStart = 20.dp,
+    topEnd = 20.dp,
+    bottomStart = 0.dp,
+    bottomEnd = 0.dp
+)
--- app/src/main/java/com/iptriana/voyage/ui/theme/Type.kt
+++ app/src/main/java/com/iptriana/voyage/ui/theme/Type.kt
@@ -2,33 +2,90 @@ package com.iptriana.voyage.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.iptriana.voyage.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+private val light = Font(R.font.raleway_light, FontWeight.W300)
+private val regular = Font(R.font.raleway_regular, FontWeight.W400)
+private val medium = Font(R.font.raleway_medium, FontWeight.W500)
+private val semibold = Font(R.font.raleway_semibold, FontWeight.W600)
+
+private val voyageFontFamily = FontFamily(fonts = listOf(light, regular, medium, semibold))
+
+val captionTextStyle = TextStyle(
+    fontFamily = voyageFontFamily,
+    fontWeight = FontWeight.W400,
+    fontSize = 16.sp
+)
+
+val voyageTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W300,
+        fontSize = 96.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 60.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 48.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 34.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 20.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = voyageFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp
     )
-    */
 )
