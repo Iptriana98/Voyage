4b57d18cab6c59a38e2f6b57c8753bad6dac92ec
Ibrahin Perez <iptrianaa@gmail.com>
2024-06-15 00:05:12 -0500

    update VoyageHome.kt

--- app/src/main/java/com/iptriana/voyage/ui/home/VoyageHome.kt
+++ app/src/main/java/com/iptriana/voyage/ui/home/VoyageHome.kt
@@ -1,7 +1,11 @@
 package com.iptriana.voyage.ui.home
 
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,7 +14,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.iptriana.voyage.data.ExploreModel
+import com.iptriana.voyage.ui.composables.ExploreSection
+import com.iptriana.voyage.ui.composables.VoyageDrawer
+import com.iptriana.voyage.ui.composables.VoyageTabBar
+import com.iptriana.voyage.ui.composables.VoyageTabs
 import kotlinx.coroutines.launch
 
 typealias OnExploreItemClicked = (ExploreModel) -> Unit
@@ -22,31 +32,32 @@ enum class VoyageScreen {
 @Composable
 fun VoyageHome(
     onExploreItemClicked: OnExploreItemClicked,
-    modifier: Modifier = Modifier,
 ) {
-    val scaffoldState = rememberScaffoldState()
-    Scaffold(
-        scaffoldState = scaffoldState,
-        modifier = Modifier.statusBarsPadding(),
-        drawerContent = {
-            VoyageDrawer()
-        }
-    ) { padding ->
-        val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-        VoyageHomeContent(
-            modifier = modifier.padding(padding),
-            onExploreItemClicked = onExploreItemClicked,
-            openDrawer = {
-                scope.launch {
-                    scaffoldState.drawerState.open()
-                }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                VoyageDrawer()
             }
-        )
+        }
+    ) {
+        Scaffold { paddingValues ->
+            VoyageHomeContent(
+                modifier = Modifier.padding(paddingValues),
+                onExploreItemClicked = onExploreItemClicked,
+                openDrawer = {
+                    scope.launch {
+                        drawerState.open()
+                    }
+                }
+            )
+        }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun VoyageHomeContent(
     onExploreItemClicked: OnExploreItemClicked,
@@ -59,46 +70,54 @@ fun VoyageHomeContent(
     val onPeopleChanged: (Int) -> Unit = { viewModel.updatePeople(it) }
     var tabSelected by remember { mutableStateOf(VoyageScreen.Fly) }
 
-    BackdropScaffold(
+    ModalNavigationDrawer(
         modifier = modifier,
-        scaffoldState = rememberBackdropScaffoldState(BackdropValue.Revealed),
-        frontLayerScrimColor = Color.Unspecified,
-        appBar = {
-            HomeTabBar(openDrawer, tabSelected, onTabSelected = { tabSelected = it })
-        },
-        backLayerContent = {
-            SearchContent(
-                tabSelected,
-                viewModel,
-                onPeopleChanged
-            )
-        },
-        frontLayerContent = {
+        drawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
+        drawerContent = {
+            ModalDrawerSheet {
+                SearchContent(
+                    tabSelected = tabSelected,
+                    onPeopleChanged = onPeopleChanged,
+                )
+            }
+        }
+    ) {
+        Scaffold(
+            modifier = modifier,
+            topBar = {
+                HomeTabBar(openDrawer, tabSelected, onTabSelected = { tabSelected = it })
+            }
+        ) { paddingValues ->
             when (tabSelected) {
                 VoyageScreen.Fly -> {
                     ExploreSection(
                         title = "Explore Flights by Destination",
                         exploreList = suggestedDestinations,
-                        onItemClicked = onExploreItemClicked
+                        onItemClicked = onExploreItemClicked,
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
+
                 VoyageScreen.Sleep -> {
                     ExploreSection(
                         title = "Explore Properties by Destination",
                         exploreList = viewModel.hotels,
-                        onItemClicked = onExploreItemClicked
+                        onItemClicked = onExploreItemClicked,
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
+
                 VoyageScreen.Eat -> {
                     ExploreSection(
                         title = "Explore Restaurants by Destination",
                         exploreList = viewModel.restaurants,
-                        onItemClicked = onExploreItemClicked
+                        onItemClicked = onExploreItemClicked,
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
             }
         }
-    )
+    }
 }
 
 @Composable
@@ -124,7 +143,7 @@ private fun HomeTabBar(
 @Composable
 private fun SearchContent(
     tabSelected: VoyageScreen,
-    viewModel: MainViewModel,
+    viewModel: MainViewModel = viewModel(),
     onPeopleChanged: (Int) -> Unit
 ) {
     when (tabSelected) {
@@ -132,9 +151,11 @@ private fun SearchContent(
             onPeopleChanged = onPeopleChanged,
             onToDestinationChanged = { viewModel.toDestinationChanged(it) }
         )
+
         VoyageScreen.Sleep -> SleepSearchContent(
             onPeopleChanged = onPeopleChanged
         )
+
         VoyageScreen.Eat -> EatSearchContent(
             onPeopleChanged = onPeopleChanged
         )
